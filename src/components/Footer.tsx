cfeb4497a22f8a54af7b8f9267c9f11c9ff0eefc
Codehouze <codehouseInc@gmail.com>
2025-10-11 13:05:40 +0200

    update socials

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -50,18 +50,18 @@ const Footer = () => {
           </p>
           
           <div className="flex items-center gap-4">
-            <a href="#" className="hover:text-primary transition-colors">
+            <a href="https://www.facebook.com/FixRiteng" className="hover:text-primary transition-colors">
               <Facebook className="w-5 h-5" />
             </a>
-            <a href="#" className="hover:text-primary transition-colors">
+            {/* <a href="#" className="hover:text-primary transition-colors">
               <Twitter className="w-5 h-5" />
-            </a>
-            <a href="#" className="hover:text-primary transition-colors">
+            </a> */}
+            <a href="https://www.instagram.com/fixrite_ng/" className="hover:text-primary transition-colors">
               <Instagram className="w-5 h-5" />
             </a>
-            <a href="#" className="hover:text-primary transition-colors">
+            {/* <a href="#" className="hover:text-primary transition-colors">
               <Linkedin className="w-5 h-5" />
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
